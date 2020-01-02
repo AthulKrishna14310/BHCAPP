83b6e7e8f6e2d27f0f4597d16990f5d5446160b8
Elson Jose <elsonjoseofficial@gmail.com>
2020-01-02 18:23:38 +0530

    Main Recyclerview done

---
+++ app/src/main/java/com/integrals/bhcapp/Helper/HomeCard.java
@@ -0,0 +1,28 @@
+package com.integrals.bhcapp.Helper;
+
+public class HomeCard {
+
+    String name,desc,topscore,icon;
+
+
+    public HomeCard(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
--- app/src/main/java/com/integrals/bhcapp/MainActivity.java
+++ app/src/main/java/com/integrals/bhcapp/MainActivity.java
@@ -1,17 +1,32 @@
 package com.integrals.bhcapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.integrals.bhcapp.Auth.LoginActivity;
+import com.integrals.bhcapp.Helper.HomeCard;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth UserAuth;
+    RecyclerView MainRV;
+    List<HomeCard> ExerciseList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         UserAuth = FirebaseAuth.getInstance();
+        MainRV = findViewById(R.id.main_RV);
+        MainRV.setHasFixedSize(true);
+        MainRV.setLayoutManager(new LinearLayoutManager(this));
+        ExerciseList = new ArrayList<>();
+
+        ExerciseList.add(new HomeCard("Hand Therapy - finger aerobics","qwertyuiopqwertyuiopqwertyuiopqwertyuiop"));
+        ExerciseList.add(new HomeCard("Eye Hand Coordination (Pattern Trace)","qwertyuiopqwertyuiopqwertyuiopqwertyuiop"));
+        ExerciseList.add(new HomeCard("Letter and Sound","qwertyuiopqwertyuiopqwertyuiopqwertyuiop"));
+        ExerciseList.add(new HomeCard("Spatial Organization","qwertyuiopqwertyuiopqwertyuiopqwertyuiop"));
+        ExerciseList.add(new HomeCard("Art with Geometry","qwertyuiopqwertyuiopqwertyuiopqwertyuiop"));
+        ExerciseList.add(new HomeCard("Letter Trace","qwertyuiopqwertyuiopqwertyuiopqwertyuiop"));
+
+        MainRV.setAdapter(new ExcerciseAdapter());
 
         CheckUserAuth();
 
@@ -32,5 +60,47 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
 
+    }
+
+    public class ExcerciseAdapter extends RecyclerView.Adapter<ExcerciseAdapter.ExerciseViewHolder>
+    {
+
+        @NonNull
+        @Override
+        public ExcerciseAdapter.ExerciseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+            View card = LayoutInflater.from(getApplicationContext()).inflate(R.layout.home_cardview_layout,parent,false);
+            return new ExcerciseAdapter.ExerciseViewHolder(card);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ExcerciseAdapter.ExerciseViewHolder holder, int position) {
+
+            holder.ExerciseName.setText(ExerciseList.get(position).getName());
+            holder.ExerciseDesc.setText(ExerciseList.get(position).getDesc());
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return ExerciseList.size();
+        }
+
+        public class ExerciseViewHolder extends RecyclerView.ViewHolder {
+
+            ImageView ExerciseIcon;
+            TextView ExerciseName,ExerciseDesc;
+            Button ExerciseTopScore;
+
+            public ExerciseViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                ExerciseIcon =  itemView.findViewById(R.id.home_card_exercise_icon_IV);
+                ExerciseName =  itemView.findViewById(R.id.home_card_exercise_name_TV);
+                ExerciseDesc =  itemView.findViewById(R.id.home_card_exercise_description_TV);
+                ExerciseTopScore =  itemView.findViewById(R.id.home_card_exercise_top_score_btn);
+
+            }
+        }
     }
 }
